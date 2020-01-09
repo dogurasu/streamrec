8f9224ac3ff517c377b18f69ca55fed9920cc5d1
Douglas <dugs134@gmail.com>
2020-01-08 18:40:57 -0800

    completed initial UI

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
+// let clientID;
+
+let clientID;
+
 fetch('clientID')
   .then(res => res.text())
-  .then(txt => console.log(txt));
+  .then(txt => {
+    console.log(txt);
+    clientID = txt;
+  });
 
-// fetch('https://api.twitch.tv/helix/streams?first=20', {
-//   headers: {
-//     'Client-ID': `${clientID}`
-//   }
-// })
-//   .then(res => res.json())
-//   .then(resJSON => console.log(resJSON))
+fetch('https://api.twitch.tv/helix/streams?first=20', {
+  headers: {
+    'Client-ID': `${clientID}`
+  }
+})
+  .then(res => res.json())
+  .then(resJSON => console.log(resJSON))
