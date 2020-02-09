e535d40a2e7134dc589927e3c2c11d01be41e9dd
dogurasu <dugs134@gmail.com>
2020-02-08 17:53:15 -0800

    commit on "master"

--- js/app.js
+++ js/app.js
@@ -1,29 +1,40 @@
 // variables
 let clientID = config.API_KEY;
-let streamInfo = {};
+let streams = {};
+let tags = {};
 
 // get Tags JSON data
+fetch('./twitchScrape/tags.json')
+  .then (res => {
+    return res.json();
+  })
+  .then (jsonData => {
+    // console.log(Object.keys(jsonData).length);
+    tags = jsonData;
+  });
 
 document.querySelector(".get-started-button").addEventListener('click', getStarted);
-let mainContent = document.querySelector("#main-content");
+let replaceItems = [document.querySelector("#main-content .branding-container"), document.querySelector("#main-content .get-started-button")]
 
 function getStarted() {
   // Replace main-content with preference checkboxes
   clearMain();
   getStreamInfo();
-  setTimeout( () => console.log(streamInfo), 1000);
+  setTimeout( () => console.log(streams), 1000);
   traverseStreamInfo();
 }
 
 const clearMain = () => {
   // clear the main-content area
-  mainContent.innerHTML = "";
-  console.log(mainContent);
+  for (i in replaceItems) {
+    replaceItems[i].style.display="none";
+  }
+  // console.log(mainContent);
 }
 
 // call Twitch API
 const getStreamInfo = () => {  
-  fetch('https://api.twitch.tv/helix/tags/streams', {
+  fetch('https://api.twitch.tv/helix/streams?first=20', {
     headers: {
       'Client-ID': `${clientID}`
     }
@@ -31,7 +42,7 @@ const getStreamInfo = () => {
     .then(res => {
       return(res.json())
     })
-    .then(resJSON => streamInfo=resJSON);
+    .then(resJSON => streams=resJSON);
 }
 
 // traverse returned JSON object
